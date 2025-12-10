c92764bac8144c163e8c6c94eb07387e4eb43420
Ram <venkataramaraju2024@gmail.com>
2025-12-10 12:31:12 +0530

    Update settings.py

--- toonai/settings.py
+++ toonai/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'accounts',
     'library',
     'recommendation',
-    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +131,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
